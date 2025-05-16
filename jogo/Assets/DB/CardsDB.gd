0b2bc38da20d2ed0a07aa6693226380317aee93c
Ju <jsn@cesar.school>
2025-05-16 14:06:55 -0300

    generic dict (again)

--- jogo/Assets/DB/CardsDB.gd
+++ jogo/Assets/DB/CardsDB.gd
@@ -1,1 +1,18 @@
 extends Node
+
+enum {carta1,carta2,carta3,carta4,carta5,carta6} 
+
+const DATA = {
+	carta1:
+		["Tipo","Tipo2"],
+	carta2:
+		["Tipo","Tipo2"],
+	carta3:
+		["Tipo1","Tipo2"],
+	carta4:
+		["Tipo1","Tipo2"],
+	carta5:
+		["Tipo1","Tipo2"],
+	carta6:
+		["Tipo1","Tipo2"]
+	}
