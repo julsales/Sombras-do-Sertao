f61f1c62924092249ae3c0a5a9beaae0b3b1f015
Jéssica Vitória <jejevitoria3@gmail.com>
2025-05-17 21:54:14 -0300

    add ao main.gd um script pra barra de equilibrio

--- jogo/main.gd
+++ jogo/main.gd
@@ -1,1 +1,9 @@
-extends Node2D
+extends Node
+
+var equilibrio := 50  # Começa equilibrado (Rompedor 50, Meninas 50)
+
+func set_equilibrio(valor: int) -> void:
+	equilibrio = clamp(valor, 0, 100)
+
+	$UI/EquilibrioBarContainer/BarraRompedor.value = equilibrio
+	$UI/EquilibrioBarContainer/BarraMeninas.value = 100 - equilibrio
