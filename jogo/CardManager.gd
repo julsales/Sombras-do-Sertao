f230145057d13ba904d65b6c7c9a967003e052fb
Ju <jsn@cesar.school>
2025-05-18 23:40:30 -0300

    drag card to card area done

--- jogo/CardManager.gd
+++ jogo/CardManager.gd
@@ -1,13 +1,17 @@
 extends Node2D
 var screen_size
 var dragging_card
-
-const  COLLISION_MASK_CARD = 1
+var is_hovering_on_card
+const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 
 # Called when the node enters the scene tree for the first time.
+#Pega o tamanho da tela do jogo
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
+# Checa se o mouse está na carta e arrasta ela de acordo com o mouse,
+# também usa o clamp pra restringir a área de onde essa carta pode ir de acordo com a posição do mouse e a posição do tamanho da tela
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if dragging_card:
@@ -16,27 +20,90 @@ func _process(delta: float) -> void:
 		clamp(mouse_pos.y,0,screen_size.y))
 
 
-#Basicamente checa se o 
+#Checa se o click esquerdo do mouse está ativo e chama a func do raycast pra saber se está clicando em uma carta, se sim, arrasta ela
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			print("Click")
-			var card = card_reycast_check()
+			var card = card_raycast_check()
 			if card:
-				dragging_card = card
+				start_drag(card)
 		else:
-			print("Left Click Released")
-			dragging_card = null
+			if dragging_card:
+				finish_drag()
+			
 
-#Basicamente checa se o mouse ta encima da carta
-func card_reycast_check():
+func start_drag(card):
+	dragging_card = card
+	card.scale = Vector2(1,1)
+	
+func finish_drag():
+	dragging_card.scale = Vector2(1.05,1.05)
+	var card_slot_found = card_slot_raycast_check()
+	if card_slot_found and not card_slot_found.card_in_slot:
+		dragging_card.position = card_slot_found.position
+		dragging_card.get_node("Area2D/CollisionShape2D").disabled = true
+		card_slot_found.card_in_slot = true
+	dragging_card = null
+
+#conecta os sinais mandados por card.gd pra esse script, melhor para garantir que todas as cartas vão funcionar da mesma forma
+func connect_card_signals(card):
+	card.connect("hovered", hover_over_card)
+	card.connect("hovered_off", hover_off_card)
+	
+func hover_over_card(card):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(card,true)
+	
+
+func hover_off_card(card):
+	if !dragging_card:
+		var hover_new_card = card_raycast_check()
+		if hover_new_card:
+			highlight_card(hover_new_card, true)
+		else:
+			is_hovering_on_card = false
+	
+func highlight_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(1.05,1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1,1)
+		card.z_index = 1
+		
+#Checa se a posição do mouse está na da posição do slot e retorna o id específico do slot
+func card_slot_raycast_check():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters) 
+	if result.size() > 0:
+		return(result[0].collider.get_parent())
+	return null
+	
+#Checa se a posição do mouse está na da posição da carta e retorna o id específico da carta
+func card_raycast_check():
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
 	parameters.position = get_global_mouse_position()
 	parameters.collide_with_areas = true
 	parameters.collision_mask = COLLISION_MASK_CARD
-	var result = space_state.intersect_point(parameters)
+	var result = space_state.intersect_point(parameters) 
 	if result.size() > 0:
-		return(result[0].collider.get_parent())
+		#return(result[0].collider.get_parent())
+		return card_highest_z_index(result)
 	return null
 	
+func card_highest_z_index(cards):
+	var highest_z_card = cards[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	for i in range(1,cards.size()):
+		var current_card = cards[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
